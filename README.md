7b5f6e6d639b18f0c247fb4487b78ccb551bef32
Christiana Holland-Jobb <christiana.holland-jobb@tecart.de>
2021-06-09 16:13:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+[![TecArt GmbH](tecart-logo-rgba_h120.png)](https://www.tecart.de)
+#TecArt Jira Login Plugin
+
+The **TecArt Jira Login** Plugin for [Grav CMS](http://github.com/getgrav/grav) to provide a Atlassian Jira basic authentication via cURL on your [Admin plugin](https://github.com/getgrav/grav-plugin-admin).
+
+To send a request using basic authentication, you'll need the following information:
+[Basic authentication](https://developer.atlassian.com/server/jira/platform/basic-authentication/).
+
+## Installation
+
+Installing the TecArt Jira Login plugin can be done in one of three ways: The GPM (Grav Package Manager) installation method lets you quickly install the plugin with a simple terminal command, the manual method lets you do so via a zip file, and the admin method lets you do so via the [Admin plugin](https://github.com/getgrav/grav-plugin-admin).
+
+### GPM Installation (Preferred)
+
+To install the plugin via the [GPM](http://learn.getgrav.org/advanced/grav-gpm), through your system's terminal (also called the command line), navigate to the root of your Grav-installation, and enter:
+
+    bin/gpm install tecart-jira-login
+
+This will install the TecArt Jira Login plugin into your `/user/plugins`-directory within [Grav CMS](http://github.com/getgrav/grav). Its files can be found under `/your/site/grav/user/plugins/tecart-jira-login`.
+
+### Manual Installation
+
+To install the plugin manually, download the zip-version of this repository and unzip it under `/your/site/grav/user/plugins`. Then rename the folder to `tecart-jira-login`.
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/tecart-jira-login
+
+> NOTE: This plugin is a modular component for [Grav CMS](http://github.com/getgrav/grav) and requires other plugins to operate:
+
+    - { name: grav, version: '>=1.7.7' }
+    - { name: login, version: '>=3.4.1' }
+    - { name: admin, version: '>=1.10.6' }
+
+### Admin Plugin
+
+You can install the plugin directly by browsing the `Plugins` menu and clicking on the `Add` button.
+
+## Configuration
+
+Here is the default configuration and an explanation of available options:
+
+```yaml
+enabled: false                                                    - Plugin is disabled by default to allow configuration
+save_as_account: false                                            - Allowed users who login to be saved to the /users/accounts directory
+jira_url: https://jira.tecart.de                                  - URL to your Jira system
+jira_api: rest/api/2/issue/createmeta                             - Jira API path - if changes are required
+admin_language: de                                                - Admin Panel language when Jira user logged in
+admin_users: frank-panser, soeren-mueller,christiana-holland-jobb - Jira User that should have admin rights in admin panel, add comma separated
+```
+
+Note that if you configure this plugin with the [Admin plugin](https://github.com/getgrav/grav-plugin-admin), a file with your configuration named tecart-jira-login.yaml will be saved in the `user/config/plugins/` folder once the configuration is saved in the Admin.
+
+Note that if you configure this plugin manually, you should copy the `user/plugins/tecart-jira-login/tecart-jira-login.yaml` to `user/config/plugins/tecart-jira-login.yaml` and only edit that copy.
+
+## Credits
+
+This is also an extension of the [Login plugin](https://github.com/getgrav/grav-plugin-login).
+
+Atlassian. Pty Ltd
+[Basic authentication](https://developer.atlassian.com/server/jira/platform/basic-authentication/).
+
+## To Do
+
+- N/A
+
+## Known Issues
+
+- N/A
---
+++ tecart-jira-login.php
@@ -0,0 +1,193 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use Grav\Common\User\User;
+use Grav\Plugin\Login\Events\UserLoginEvent;
+use Grav\Plugin\Login\Login;
+
+class TecartJiraLoginPlugin extends Plugin
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(){
+        return [
+            'onPluginsInitialized' => [
+                ['onPluginsInitialized', 0]
+            ],
+            'onUserLoginAuthenticate'   => ['userLoginAuthenticate', 1000],
+            'onUserLoginFailure'        => ['userLoginFailure', 0],
+            'onUserLogin'               => ['userLogin', 0],
+            'onUserLogout'              => ['userLogout', 0],
+        ];
+    }
+
+    public function onPluginsInitialized(){
+
+        // Check to ensure admin plugin is enabled.
+        if (!$this->grav['config']->get('plugins.admin.enabled')) {
+            throw new \RuntimeException('The Admin plugin needs to be installed and enabled');
+        }
+        // Check to ensure login plugin is enabled.
+        if (!$this->grav['config']->get('plugins.login.enabled')) {
+            throw new \RuntimeException('The Login plugin needs to be installed and enabled');
+        }
+    }
+
+    public function userLoginAuthenticate(UserLoginEvent $event){
+
+        // event is called when login button is pressed
+        // credentials contain username and password in array Array ( [username] => [password] => )
+        $credentials = $event->getCredentials();
+
+        // if empty username -> ignore
+        if($credentials['username'] == '' or $credentials['password'] == ''){
+            $event->setStatus($event::AUTHENTICATION_FAILURE);
+            return;
+        }
+
+        $username = $credentials['username'];
+        $password = $credentials['password'];
+
+        // Plugin parameters
+        $jira_url           = $this->config->get('plugins.tecart-jira-login.jira_url');
+        $jira_api           = $this->config->get('plugins.tecart-jira-login.jira_api') ?: 'rest/api/2/issue/createmeta';
+        $save_as_account    = $this->config->get('plugins.tecart-jira-login.save_as_account');
+
+        if (is_null($jira_url)) {
+            throw new ConnectionException('FATAL: Jira URL entry missing in plugin configuration.');
+        }
+
+        // set cURL url - https://jira.tecart.de/rest/api/2/issue/createmeta
+        $ch_url = $jira_url.'/'.$jira_api;
+
+        // do the Jira Basic Authentification
+        $request = $this->jiraBasicAuthentication($ch_url, $username, $password);
+
+        // if cURL error
+        if (isset($request['error'])) {
+
+            $this->grav['log']->error('plugin.tecart-jira-login: ' .  $username . ' - ' . $request['error']);
+
+            // Just return so other authenticators can take a shot...
+            return;
+        }
+
+        // if authentication success
+       if(isset($request['http_code'])  &&  $request['http_code'] <= 299){
+            // Create Grav User (no saving at this point - just serve user infos to make grav admin work)
+            $grav_user = $this->setGravUser($username);
+
+            // Save user
+            if ($save_as_account === true) {
+                $grav_user->save();
+            }
+
+            // Login
+            $event->setUser($grav_user);
+
+            $event->setStatus($event::AUTHENTICATION_SUCCESS);
+            $event->stopPropagation();
+        }
+        // if authentication error
+        else{
+            $event->setStatus($event::AUTHENTICATION_FAILURE);
+            $event->stopPropagation();
+        }
+    }
+
+    public function userLoginFailure(UserLoginEvent $event){
+        // This gets fired if user fails to log in.
+    }
+
+    public function userLogin(UserLoginEvent $event){
+        // This gets fired if user successfully logs in.
+    }
+
+    public function userLogout(UserLoginEvent $event){
+        // This gets fired on user logout.
+    }
+
+    //https://developer.atlassian.com/server/jira/platform/basic-authentication/
+    protected function jiraBasicAuthentication($url="", $user="", $password=""){
+
+        $ch = curl_init();
+
+        $token = base64_encode($user.":".$password);
+
+        $headers = array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: Basic '.$token
+        );
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION , true);
+        curl_setopt($ch, CURLOPT_USERPWD, $user.":".$password);
+
+        $curl = curl_exec($ch);
+        //print_r($curl);
+
+        $ch_error = curl_error($ch);
+
+        if ($ch_error) {
+            $result = array('error' => 'cURL Error: '.$ch_error);
+        }
+        else{
+            $result = curl_getinfo($ch);
+        }
+
+        curl_close($ch);
+
+        return $result;
+    }
+
+    protected function setGravUser($username) {
+
+        // Create Grav User
+        $grav_user = User::load(strtolower($username));
+
+        // Plugin Settings
+        $admins             = $this->config->get('plugins.tecart-jira-login.admin_users');
+        $admin_language     = $this->config->get('plugins.tecart-jira-login.admin_language') ?: 'de';
+
+        // Set permissions
+        $permissions = array();
+
+        $permissions['site']['login'] = true;
+        $permissions['admin']['login'] = true;
+        $permissions['admin']['pages'] = true;
+        $permissions['admin']['cache'] = true;
+        $permissions['admin']['configuration_site'] = true;
+
+        // usernames that should act as admin, remove whitespaces
+        $admins_array =explode(',',preg_replace('/\s+/', '', $admins));
+
+        // if username is admin
+        if(in_array($username, $admins_array)){
+            $permissions['admin']['super'] = true;
+            $permissions['admin']['configuration'] = true;
+            $permissions['admin']['maintenance'] = true;
+            $permissions['admin']['statistics'] = true;
+            $permissions['admin']['plugins'] = true;
+            $permissions['admin']['themes'] = true;
+            $permissions['admin']['tools'] = true;
+            $permissions['admin']['users'] = true;
+            $permissions['admin']['flex-objects'] = true;
+        }
+
+        // Set user
+        //$grav_user['fullname']  = 'Jira User';
+        $grav_user['fullname']  = $username;        //is shown in admin panel
+        $grav_user['email']     = 'jira@tecart.de';
+        $grav_user['language']  = $admin_language;
+        $grav_user['access']    = $permissions;
+
+        return $grav_user;
+    }
+}
